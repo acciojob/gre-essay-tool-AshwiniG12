80514fd529655c6b8f8e8c15ca1644336ec7ee58
AshwiniG12 <121161369+AshwiniG12@users.noreply.github.com>
2023-03-01 19:49:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //your code here
-const textbox = document.getElementById("evaluatedText");
-  const wordCountDisplay = document.getElementById("wordCount");
-
-  textbox.addEventListener("input", function() {
-    const wordCount = textbox.value.split(" ").length;
-    wordCountDisplay.innerText = wordCount === 0 ? "0" : wordCount;
+function renderText(domElt, text) {
+  domElt.innerText = text;
+}
+ 
+function handleWord(text) {
+  const textArr = text.split(" ");
+  let wordCount = 0;
+ 
+  for (word of textArr) {
+    if (/[a-zA-Z0-9]/.test(word)) {
+      wordCount += 1;
+    }
+  }
+ 
+  return wordCount;
+}
+ 
+function handleChar(text) {
+  return text.length;
+}
+ 
+function init() {
+  const textArea = document.querySelector("textarea");
+  const charDOM = document.querySelector("#charCount");
+  const wordDOM = document.querySelector("#wordCount");
+ 
+  textArea.addEventListener("input", (event) => {
+    const text = event.target.value.trim();
+ 
+    renderText(wordDOM, handleWord(text));
+    renderText(charDOM, handleChar(text));
   });
+}
+ 
+init();
+
+
   
